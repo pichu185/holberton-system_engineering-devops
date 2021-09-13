ac9e8bdfdf61b3313c3310acf738a47ff2a518b4
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-09-13 18:12:51 -0300

    2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
 """Export to JSON"""
 
-from sys import argv
-import requests
 import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = requests.get(url + "users/{}".format(argv[1])).json()
+    all = requests.get(url + "todos", params={"userId": argv[1]}).json()
+    completed_tasks = []
+    for task in all:
+        task_dic = {}
+        task_dic["task"] = task.get("title")
+        task_dic["completed"] = task.get("completed")
+        task_dic["username"] = user.get("username")
+        completed_tasks.append(task_dic)
+    json_obj = {}
+    json_obj[argv[1]] = completed_tasks
+    with open(argv[1] + ".json", "w") as jsonFile:
+        json.dump(json_obj, jsonFile)
