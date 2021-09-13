b298b3a45ec11733c06b2937e1a4c99669694bd3
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-09-13 18:38:32 -0300

    3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,6 +1,27 @@
 #!/usr/bin/python3
-"""Dictionary of list of dictionaries"""
+"""Export to JSON"""
 
-from sys import argv
-import requests
 import json
+import requests
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users").json()
+    all = requests.get(url + "todos").json()
+    user_dict = {}
+    name_dict = {}
+    for field in user:
+        user_id = field.get("id")
+        user_dict[user_id] = []
+        name_dict[user_id] = field.get("username")
+
+    for task in all:
+        user_id = task.get("userId")
+        task_dic = {}
+        task_dic["task"] = task.get("title")
+        task_dic["completed"] = task.get("completed")
+        task_dic["username"] = name_dict.get(user_id)
+        user_dict.get(user_id).append(task_dic)
+
+    with open("todo_all_employees" + ".json", "w") as jsonFile:
+        json.dump(user_dict, jsonFile)
