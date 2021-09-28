5121d70155c33adac788945e311daa54f3cf661a
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-09-28 18:41:15 -0300

    1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+Write a function that queries the Reddit API and prints the titles of the
+first 10 hot posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    function that queries the Reddit API and prints the titles of
+    the first 10 hot posts
+    """
+    url = "https://www.reddit.com/r/'+subreddit+'/hot.json"
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        for i in range(10):
+            hot = response.json()['data']['children'][i]['data']['title']
+            print(hot)
+    print("None")
