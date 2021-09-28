dd52f82db32a33be4d07ad79727934418e01d71e
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-09-28 18:21:21 -0300

    0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and returns the number of
+subscribers (not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """number of subscribers"""
+    url = "https://www.reddit.com/r/'+subreddit+'/about.json"
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        users = response.json().get("data")
+        return users.get("subscribers")
+    else
+        return 0
