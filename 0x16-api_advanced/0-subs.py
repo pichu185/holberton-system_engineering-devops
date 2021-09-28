1da36ac3090080599693364a738c1da8c7c81e0f
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-09-28 18:30:21 -0300

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and returns the number of subscri"""
+'''function that queries the Reddit API and returns the number of subscri'''
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """number of subscribers"""
+    '''number of subscribers'''
     url = "https://www.reddit.com/r/'+subreddit+'/about.json"
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
