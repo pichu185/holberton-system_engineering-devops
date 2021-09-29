0240a52e70260e7b06153c69e90901f3942fdb60
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-09-28 21:46:35 -0300

    1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,8 +11,9 @@ def top_ten(subreddit):
     function that queries the Reddit API and prints the titles of
     the first 10 hot posts
     """
-    url = "https://www.reddit.com/r/'+subreddit+'/hot.json"
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(url, headers={'User-Agent': 'Pichu'},
+                            allow_redirects=False)
     if response.status_code == 200:
         for i in range(10):
             hot = response.json()['data']['children'][i]['data']['title']
