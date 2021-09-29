869e1c473783d81d22441287d88a6a595c2c27f7
Pichu185 <pichu_otegui@hotmail.com>
2021-09-29 00:30:47 +0000

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,8 @@ import requests
 def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
     url = "https://www.reddit.com/r/'+subreddit+'/about.json"
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers={'User-Agent': 'Pichu'},
+                            allow_redirects=False)
     if response.status_code == 200:
         users = response.json().get('data')
         return users.get('subscribers')
